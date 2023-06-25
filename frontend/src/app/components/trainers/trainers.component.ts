0e5f720563ef80eb6f1b0e1bd2270209734cfe50
dulemor <tadicdusan99@gmail.com>
2023-06-25 12:34:59 +0200

    dodati treneri i cenovnik

--- frontend/src/app/components/trainers/trainers.component.ts
+++ frontend/src/app/components/trainers/trainers.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user-services.service';
 
 @Component({
   selector: 'app-trainers',
   templateUrl: './trainers.component.html',
-  styleUrls: ['./trainers.component.css']
+  styleUrls: ['./trainers.component.css'],
 })
-export class TrainersComponent {
+export class TrainersComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.getAllUsers().then((resp) => {
+      let userString = JSON.stringify(resp);
+      this.allUsers = JSON.parse(userString);
+    });
+
+    for (let i = 0; i < this.allUsers.length; i++) {
+      console.log(i);
+    }
+
+    for (let user of this.allUsers) {
+      console.log(user.username);
+      /*if (user.userType == 3 && user.approved == true) {
+        this.allTrainers.push(user);
+      }*/
+    }
+  }
+
+  allUsers: User[] = [];
+  allTrainers: User[] = [];
 }
